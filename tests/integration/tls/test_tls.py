66bad0da204aa0060c367b4c8b82981fd3505278
MiaAltieri <mgaltier200@gmail.com>
2024-08-23 12:30:05 +0000

    all tls tests are working

--- tests/integration/tls/test_tls.py
+++ tests/integration/tls/test_tls.py
@@ -8,21 +8,11 @@ from .helpers import (
     check_mongos_tls_enabled,
     check_mongos_tls_disabled,
     toggle_tls_mongos,
-    EXTERNAL_CERT_PATH,
-    INTERNAL_CERT_PATH,
-    get_file_contents,
-    check_certs_correctly_distributed,
-    time_file_created,
-    time_process_started,
     deploy_tls,
     integrate_mongos_with_tls,
     integrate_cluster_with_tls,
     MONGOS_APP_NAME,
-    CERT_REL_NAME,
     CERTS_APP_NAME,
-    CONFIG_SERVER_APP_NAME,
-    SHARD_APP_NAME,
-    CLUSTER_COMPONENTS,
     rotate_and_verify_certs,
 )
 
@@ -101,32 +91,26 @@ async def test_tls_reenabled(ops_test: OpsTest) -> None:
     await check_mongos_tls_enabled(ops_test)
 
 
-# @pytest.mark.group(1)
-# @pytest.mark.abort_on_fail
-# async def test_mongos_tls_ca_mismatch(ops_test: OpsTest) -> None:
-#     """Tests that mongos charm can disable TLS."""
-#     await toggle_tls_mongos(ops_test, enable=False)
-#     await ops_test.model.deploy(
-#         CERTS_APP_NAME, application_name=DIFFERENT_CERTS_APP_NAME, channel="stable"
-#     )
-#     await ops_test.model.wait_for_idle(
-#         apps=[DIFFERENT_CERTS_APP_NAME],
-#         idle_period=10,
-#         raise_on_blocked=False,
-#         status="active",
-#         timeout=TIMEOUT,
-#     )
+@pytest.mark.group(1)
+@pytest.mark.abort_on_fail
+async def test_mongos_tls_ca_mismatch(ops_test: OpsTest) -> None:
+    """Tests that mongos charm can disable TLS."""
+    await toggle_tls_mongos(ops_test, enable=False)
+    await ops_test.model.deploy(
+        CERTS_APP_NAME, application_name=DIFFERENT_CERTS_APP_NAME, channel="stable"
+    )
+    await ops_test.model.wait_for_idle(
+        apps=[DIFFERENT_CERTS_APP_NAME],
+        idle_period=10,
+        raise_on_blocked=False,
+        status="active",
+        timeout=TIMEOUT,
+    )
 
-#     await toggle_tls_mongos(ops_test, enable=True, certs_app_name=DIFFERENT_CERTS_APP_NAME)
-
-#     await ops_test.model.wait_for_idle(
-#         apps=[MONGOS_APP_NAME],
-#         idle_period=20,
-#         raise_on_blocked=False,
-#         timeout=TIMEOUT,
-#     )
-
-#     mongos_unit = ops_test.model.applications[MONGOS_APP_NAME].units[0]
-#     assert (
-#         mongos_unit.workload_status_message == "mongos CA and Config-Server CA don't match."
-#     ), "mongos fails to report mismatch in CA."
+    await toggle_tls_mongos(ops_test, enable=True, certs_app_name=DIFFERENT_CERTS_APP_NAME)
+    await wait_for_mongos_units_blocked(
+        ops_test,
+        MONGOS_APP_NAME,
+        status="mongos CA and Config-Server CA don't match.",
+        timeout=300,
+    )
