cd1fa10ab38f2fcbc51e3b7ceb62d464a1fe9141
Mia Altieri <mgaltier200@gmail.com>
2024-08-29 07:34:21 +0000

    more infomrative log messages in tests

--- tests/integration/client_relations/helpers.py
+++ tests/integration/client_relations/helpers.py
@@ -21,6 +21,7 @@ from ..helpers import (
 )
 
 from pytest_operator.plugin import OpsTest
+from pymongo.errors import ServerSelectionTimeoutError
 
 
 PORT_MAPPING_INDEX = 4
@@ -81,12 +82,18 @@ def is_relation_joined(ops_test: OpsTest, endpoint_one: str, endpoint_two: str) 
     return False
 
 
-def get_port_from_node_port(ops_test: OpsTest, node_port_name: str) -> str:
+def get_node_port_info(ops_test: OpsTest, node_port_name: str) -> str:
     node_port_cmd = f"kubectl get svc  -n  {ops_test.model.name} |  grep NodePort | grep {node_port_name}"
-    result = subprocess.run(node_port_cmd, shell=True, capture_output=True, text=True)
-    if result.returncode:
-        logger.info("was not able to find nodeport")
-        assert False, f"Command: {node_port_cmd} to find node port failed."
+    return subprocess.run(node_port_cmd, shell=True, capture_output=True, text=True)
+
+
+def has_node_port(ops_test: OpsTest, node_port_name: str) -> None:
+    result = get_node_port_info(ops_test, node_port_name)
+    return len(result.stdout.splitlines()) > 0
+
+
+def get_port_from_node_port(ops_test: OpsTest, node_port_name: str) -> str:
+    result = get_node_port_info(ops_test, node_port_name)
 
     assert (
         len(result.stdout.splitlines()) > 0
@@ -99,42 +106,75 @@ def get_port_from_node_port(ops_test: OpsTest, node_port_name: str) -> str:
     return port_mapping.split(":")[1].split("/")[0]
 
 
-def assert_node_port_available(ops_test: OpsTest, node_port_name: str) -> None:
-    assert get_port_from_node_port(
-        ops_test, node_port_name
-    ), "No port information for expected service"
+def assert_node_port_availablity(
+    ops_test: OpsTest, node_port_name: str, available: bool = True
+) -> None:
+    incorrect_availablity = "not available" if available else "is available"
+    assert (
+        has_node_port(ops_test, node_port_name) == available
+    ), f"Port information {incorrect_availablity} for service"
 
 
 async def assert_all_unit_node_ports_available(ops_test: OpsTest):
     """Assert all ports available in mongos deployment."""
     for unit_id in range(len(ops_test.model.applications[MONGOS_APP_NAME].units)):
-        assert_node_port_available(
+        assert_node_port_availablity(
             ops_test, node_port_name=f"{MONGOS_APP_NAME}-{unit_id}-external"
         )
 
         exposed_node_port = get_port_from_node_port(
             ops_test, node_port_name=f"{MONGOS_APP_NAME}-{unit_id}-external"
         )
-        public_k8s_ip = get_public_k8s_ip()
-        username, password = await get_mongos_user_password(ops_test, MONGOS_APP_NAME)
+
+        assert await is_external_mongos_client_reachble(
+            ops_test, exposed_node_port
+        ), "client is not reachable"
+
+
+async def is_external_mongos_client_reachble(
+    ops_test: OpsTest, exposed_node_port: str
+) -> bool:
+    """Returns True if the mongos client is reachable on the provided node port via the k8s ip."""
+    public_k8s_ip = get_public_k8s_ip()
+    username, password = await get_mongos_user_password(ops_test, MONGOS_APP_NAME)
+    try:
         external_mongos_client = MongoClient(
             f"mongodb://{username}:{password}@{public_k8s_ip}:{exposed_node_port}"
         )
         external_mongos_client.admin.command("usersInfo")
+    except ServerSelectionTimeoutError:
+        return False
+    finally:
         external_mongos_client.close()
 
+    return True
 
-@retry(stop=stop_after_attempt(10), wait=wait_fixed(1), reraise=True)
+
+async def assert_all_unit_node_ports_are_unavailable(ops_test: OpsTest):
+    """Assert all ports available in mongos deployment."""
+    for unit_id in range(len(ops_test.model.applications[MONGOS_APP_NAME].units)):
+        assert_node_port_availablity(
+            ops_test,
+            node_port_name=f"{MONGOS_APP_NAME}-{unit_id}-external",
+            available=False,
+        )
+
+
 def get_public_k8s_ip() -> str:
     result = subprocess.run(
-        "sudo kubectl get nodes", shell=True, capture_output=True, text=True
+        "kubectl get nodes", shell=True, capture_output=True, text=True
     )
 
     if result.returncode:
-        logger.info("failed to retrieve public facing k8s IP")
+        logger.info("failed to retrieve public facing k8s IP error: %s", result.stderr)
+        assert False, "failed to retrieve public facing k8s IP"
+
+    if len(result.stdout.splitlines()) < 2:
+        logger.info("No entries for public facing k8s IP, : %s", result.stdout)
         assert False, "failed to retrieve public facing k8s IP"
 
     # port information is the first item of the last line
+    logger.info("Retrieved port information: %s", result.stdout)
     port_mapping = result.stdout.splitlines()[-1].split()[0]
 
     # port mapping is of the form ip-172-31-18-133
