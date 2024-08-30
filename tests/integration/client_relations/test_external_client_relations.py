1abb6c5d0536df90e4b5998a1efe139058b2bd71
Mia Altieri <mgaltier200@gmail.com>
2024-08-30 15:22:05 +0000

    test is failing due to separate issue, will be addressed in separate ticket

--- tests/integration/client_relations/test_external_client_relations.py
+++ tests/integration/client_relations/test_external_client_relations.py
@@ -33,21 +33,21 @@ TEST_USER_PWD = "Test123"
 TEST_DB_NAME = "my-test-db"
 
 
-# @pytest.mark.group(1)
-# @pytest.mark.abort_on_fail
-# async def test_build_and_deploy(ops_test: OpsTest):
-#     """Build and deploy a sharded cluster."""
-#     await deploy_cluster_components(ops_test)
-#     await build_cluster(ops_test)
+@pytest.mark.group(1)
+@pytest.mark.abort_on_fail
+async def test_build_and_deploy(ops_test: OpsTest):
+    """Build and deploy a sharded cluster."""
+    await deploy_cluster_components(ops_test)
+    await build_cluster(ops_test)
 
-#     await deploy_client_app(ops_test, external=False)
-#     await integrate_client_app(ops_test, client_app_name=APPLICATION_APP_NAME)
+    await deploy_client_app(ops_test, external=False)
+    await integrate_client_app(ops_test, client_app_name=APPLICATION_APP_NAME)
 
-#     await deploy_client_app(ops_test, external=True)
-#     await ops_test.model.applications[DATA_INTEGRATOR_APP_NAME].set_config(
-#         {"database-name": "test-database"}
-#     )
-#     await integrate_client_app(ops_test, client_app_name=DATA_INTEGRATOR_APP_NAME)
+    await deploy_client_app(ops_test, external=True)
+    await ops_test.model.applications[DATA_INTEGRATOR_APP_NAME].set_config(
+        {"database-name": "test-database"}
+    )
+    await integrate_client_app(ops_test, client_app_name=DATA_INTEGRATOR_APP_NAME)
 
 
 @pytest.mark.group(1)
@@ -57,7 +57,9 @@ async def test_mongos_external_connections(ops_test: OpsTest) -> None:
     configuration_parameters = {"expose-external": "nodeport"}
 
     # apply new configuration options
-    await ops_test.model.applications[MONGOS_APP_NAME].set_config(configuration_parameters)
+    await ops_test.model.applications[MONGOS_APP_NAME].set_config(
+        configuration_parameters
+    )
     await ops_test.model.wait_for_idle(apps=[MONGOS_APP_NAME], idle_period=15)
 
     # verify each unit has a node port available
@@ -65,6 +67,7 @@ async def test_mongos_external_connections(ops_test: OpsTest) -> None:
 
 
 @pytest.mark.group(1)
+@pytest.mark.skip("Add in once DPE-5314 is addressed.")
 @pytest.mark.abort_on_fail
 async def test_mongos_external_connections_scale(ops_test: OpsTest) -> None:
     """Tests that new mongos units are accessible externally."""
@@ -82,7 +85,9 @@ async def test_mongos_bad_configuration(ops_test: OpsTest) -> None:
     configuration_parameters = {"expose-external": "nonsensical-setting"}
 
     # apply new configuration options
-    await ops_test.model.applications[MONGOS_APP_NAME].set_config(configuration_parameters)
+    await ops_test.model.applications[MONGOS_APP_NAME].set_config(
+        configuration_parameters
+    )
 
     # verify that Charmed Mongos is blocked and reports incorrect credentials
     await wait_for_mongos_units_blocked(
@@ -97,8 +102,12 @@ async def test_mongos_bad_configuration(ops_test: OpsTest) -> None:
 
     # reset config for other tests
     configuration_parameters = {"expose-external": "nodeport"}
-    await ops_test.model.applications[MONGOS_APP_NAME].set_config(configuration_parameters)
-    await ops_test.model.wait_for_idle(apps=[MONGOS_APP_NAME], status="active", idle_period=15)
+    await ops_test.model.applications[MONGOS_APP_NAME].set_config(
+        configuration_parameters
+    )
+    await ops_test.model.wait_for_idle(
+        apps=[MONGOS_APP_NAME], status="active", idle_period=15
+    )
 
 
 @pytest.mark.group(1)
@@ -109,10 +118,14 @@ async def test_external_clients_use_nodeport(ops_test: OpsTest) -> None:
         ops_test, app_name=DATA_INTEGRATOR_APP_NAME, relation_name="mongodb"
     )
 
-    assert get_public_k8s_ip() in external_uri, "External client URI does not have public IP."
+    assert (
+        get_public_k8s_ip() in external_uri
+    ), "External client URI does not have public IP."
 
     for host in get_k8s_local_mongodb_hosts(ops_test):
-        assert host not in external_uri, "External client should not have local hosts in URI."
+        assert (
+            host not in external_uri
+        ), "External client should not have local hosts in URI."
 
 
 @pytest.mark.group(1)
@@ -140,7 +153,9 @@ async def test_mongos_disable_external_connections(ops_test: OpsTest) -> None:
     configuration_parameters = {"expose-external": "none"}
 
     # apply new configuration options
-    await ops_test.model.applications[MONGOS_APP_NAME].set_config(configuration_parameters)
+    await ops_test.model.applications[MONGOS_APP_NAME].set_config(
+        configuration_parameters
+    )
     await ops_test.model.wait_for_idle(
         apps=[MONGOS_APP_NAME, DATA_INTEGRATOR_APP_NAME], idle_period=15
     )
